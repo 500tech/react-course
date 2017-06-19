ba66eb5afbe9efe7dbaa94bfc97cc687f46197a9
Boris Dinkevich <your_email@example.com>
2017-06-19 13:35:53 +0300

    Added Header and Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,14 @@
 import React from 'react';
-import Recipes from './Recipes';
-import AddRecipe from './AddRecipe';
-import { getID } from '../lib/utils';
+import Header from './Header';
+import Footer from './Footer';
+import RecipesView from './RecipesView';
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      recipes: [
-        { id: getID(), title: 'Waffles', favorite: true },
-        { id: getID(), title: 'Omelette', favorite: false }
-      ]
-    };
-  }
-
-  addRecipe = (title) => {
-    const newRecipe = {
-      id: getID(),
-      title,
-      favorite: false
-    };
-
-    const newRecipes = this.state.recipes.concat(newRecipe);
-
-    this.setState({ recipes: newRecipes });
-  };
-
-  toggleRecipes = (recipe) => {
-    recipe.favorite = !recipe.favorite;
-    this.forceUpdate();
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Recipes:</h1>
-        <Recipes recipes={ this.state.recipes } toggle={ this.toggleRecipes } />
-        <AddRecipe addRecipe={ this.addRecipe } />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Header />
+    <RecipesView />
+    <Footer />
+  </div>
+);
 
 export default App;
---
+++ src/components/RecipesView.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Recipes from './Recipes';
+import AddRecipe from './AddRecipe';
+import { getID } from '../lib/utils';
+
+class RecipesView extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recipes: [
+        { id: getID(), title: 'Waffles', favorite: true },
+        { id: getID(), title: 'Omelette', favorite: false }
+      ]
+    };
+  }
+
+  addRecipe = (title) => {
+    const newRecipe = {
+      id: getID(),
+      title,
+      favorite: false
+    };
+
+    const newRecipes = this.state.recipes.concat(newRecipe);
+
+    this.setState({ recipes: newRecipes });
+  };
+
+  toggleRecipes = (recipe) => {
+    recipe.favorite = !recipe.favorite;
+    this.forceUpdate();
+  };
+
+  render() {
+    return (
+      <div className="main">
+        <div className="pane">
+          <Recipes recipes={ this.state.recipes } toggle={ this.toggleRecipes }/>
+        </div>
+
+        <div className="pane">
+          <AddRecipe addRecipe={ this.addRecipe }/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RecipesView;
