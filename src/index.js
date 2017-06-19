e0a316c47e6d7b5c837bae61a41a65189d776767
Boris Dinkevich <your_email@example.com>
2017-06-19 13:18:00 +0300

    Added JSX

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 
+const recipes = ['Waffles', 'Omelette'];
+
+const Recipe = () => (
+  <li>Waffel</li>
+);
+
+const App = () => (
+  <div>
+    <h1>Recipes:</h1>
+    <ul>
+      <Recipe />
+      <Recipe />
+    </ul>
+  </div>
+);
+
 render(
-  React.createElement('div', null, 'Hello world!'),
+  React.createElement(App),
   document.getElementById('root')
 );
