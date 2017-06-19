1da0a37422a9c17c13c91a433d8ba1f7b09a5e50
Boris Dinkevich <your_email@example.com>
2017-06-19 13:20:24 +0300

    Split into multiple components

--- src/index.js
+++ src/index.js
@@ -12,6 +12,25 @@ const Recipes = ({ recipes }) => (
   </ul>
 );
 
+class AddRecipe extends React.Component {
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.props.addRecipe(this.title.value);
+
+    this.title.value = '';
+  }
+
+  render() {
+    return (
+      <form onSubmit={ this.onSubmit.bind(this) }>
+        <input ref={ e => this.title = e } type="text"/>
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
 class App extends React.Component {
   constructor() {
     super();
@@ -21,32 +40,23 @@ class App extends React.Component {
     };
   }
 
+  addRecipe = (title) => {
+    const newRecipes = this.state.recipes.concat(title);
+    this.setState({ recipes: newRecipes });
+  };
+
   render() {
     return (
       <div>
         <h1>Recipes:</h1>
         <Recipes recipes={ this.state.recipes }/>
-
-        <form onSubmit={ this.onSubmit.bind(this) }>
-          <input ref={ e => this.title = e } type="text"/>
-          <button>Add</button>
-        </form>
+        <AddRecipe addRecipe={ this.addRecipe } />
       </div>
     );
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-
-    const newRecipes = this.state.recipes.concat(this.title.value);
-
-    this.title.value = '';
-
-    this.setState({ recipes: newRecipes });
   }
 }
 
 render(
-  <App />a,
+  React.createElement(App),
   document.getElementById('root')
 );
