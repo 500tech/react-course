3c3fa40751af3ada50625aeffbd49e479ebb6686
Boris Dinkevich <your_email@example.com>
2017-06-19 13:40:39 +0300

    Added redux

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Recipe from './Recipe';
 
 const Recipes = ({ recipes, toggle }) => (
@@ -13,4 +14,11 @@ Recipes.propTypes = {
   toggle: PropTypes.func.isRequired
 };
 
-export default Recipes;
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.recipes,
+    toggle: () => console.log("Toggle")
+  }
+};
+
+export default connect(mapStateToProps)(Recipes);
---
+++ src/store.js
@@ -0,0 +1,38 @@
+import { getID } from './lib/utils';
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+  console.log('Got Action ' + action.type, action);
+
+  switch (action.type) {
+    case 'ADD_RECIPE':
+      const newRecipe = {
+        id: getID(),
+        title: action.title,
+        favorite: false
+      };
+
+      const newRecipes = state.recipes.concat(newRecipe);
+
+      return Object.assign({}, state, {
+        recipes: newRecipes
+      });
+
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  recipes: [
+    { id: getID(), title: 'Waffles', favorite: false },
+    { id: getID(), title: 'Omelette', favorite: true },
+    { id: getID(), title: 'Dog Food', favorite: true }
+  ]
+};
+
+const store = createStore(reducer, initialState);
+
+window.store = store;
+
+export default store;
