56d0386b521a6ca2b7b2c333b78ec75c7bf507c1
Boris Dinkevich <your_email@example.com>
2017-06-19 13:19:15 +0300

    Implemented add recipe

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 
-const recipes = ['Waffles', 'Omelette'];
-
 const Recipe = ({ recipe }) => (
   <li>{ recipe }</li>
 );
@@ -14,14 +12,41 @@ const Recipes = ({ recipes }) => (
   </ul>
 );
 
-const App = () => (
-  <div>
-    <h1>Recipes:</h1>
-    <Recipes recipes={ recipes } />
-  </div>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recipes: ['Waffles', 'Omelette']
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Recipes:</h1>
+        <Recipes recipes={ this.state.recipes }/>
+
+        <form onSubmit={ this.onSubmit.bind(this) }>
+          <input ref={ e => this.title = e } type="text"/>
+          <button>Add</button>
+        </form>
+      </div>
+    );
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newRecipes = this.state.recipes.concat(this.title.value);
+
+    this.title.value = '';
+
+    this.setState({ recipes: newRecipes });
+  }
+}
 
 render(
-  React.createElement(App),
+  <App />a,
   document.getElementById('root')
 );
