6ee81b6587f1e323910a493bd3ddcc11bfa7b59b
Boris Dinkevich <your_email@example.com>
2017-10-17 11:44:34 +0300

    Temp

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 
+const Header = (props) => {
+  return React.createElement('h1', null, props.text)
+};
+
+const App = () => {
+  return React.createElement('div', null,
+    React.createElement(Header, { text: 'Hi' }),
+    React.createElement(Header, { text: 'Bye' }),
+    React.createElement('h2', null, 'World')
+  )
+};
+
+window.App = App;
+
+
 render(
-  React.createElement('div', null, 'Hello world!'),
+  React.createElement(App),
   document.getElementById('root')
 );
