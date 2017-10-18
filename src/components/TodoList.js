6d423f4c8eee609707df3b979e4e63ca957161aa
Boris Dinkevich <your_email@example.com>
2017-10-18 11:43:07 +0300

    Temp

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Todo from './Todo';
 
-const TodoList = () => (
-  <ul className="todo-list">
-    <Todo completed={ true } label="Conver to JSX" />
-    <Todo completed={ false } label="Divide into Components" />
-  </ul>
-);
+class TodoList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todos: [
+        { title: 'Convert to JSX', completed: true },
+        { title: 'Divide into Components', completed: true },
+        { title: 'Add state', completed: false }
+      ]
+    };
+  }
+  
+  render() {
+    return (
+      <ul className="todo-list">
+        {
+          this.state.todos.map(todo => <Todo key={ todo.title } todo={ todo } />)
+        }
+      </ul>
+    );
+  }
+}
 
 TodoList.propTypes = {
 };
