6ff722789c41bba2c2a7e1a9a05174905a83468b
Vova Porton <vova@500tech.com>
2017-11-27 15:58:08 +0200

    Nested navigation example

--- src/native-components/root.js
+++ src/native-components/root.js
@@ -1,50 +1,40 @@
 import React from 'react';
 import { View, StyleSheet, Text, Platform, TouchableOpacity } from 'react-native';
-import { StackNavigator, TabNavigator, DrawerNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
 // COMPONENTS
-const FirstComponent = ({ navigation }) => (
-  <TouchableOpacity onPress={ () => navigation.navigate('Second') }>
-    <Text>First component!</Text>
+const FirstTabComponent = ({ navigation }) => (
+  <TouchableOpacity onPress={ () => navigation.navigate('SecondTab') }>
+    <Text>First tab component!</Text>
   </TouchableOpacity>
 );
 
-const SecondComponent = ({ navigation }) => (
-  <TouchableOpacity onPress={ () => navigation.navigate('Third') }>
-    <Text>Second component!</Text>
+const FirstStackComponent = ({ navigation }) => (
+  <TouchableOpacity onPress={ () => navigation.navigate('SecondStack') }>
+    <Text>First stack component!</Text>
   </TouchableOpacity>
 );
 
-const ThirdComponent = ({ navigation }) => (
-  <TouchableOpacity onPress={ () => navigation.navigate('First') }>
-    <Text>Third component!</Text>
+const SecondStackComponent = ({ navigation }) => (
+  <TouchableOpacity onPress={ () => navigation.navigate('FirstTab') }>
+    <Text>Second stack component!</Text>
   </TouchableOpacity>
 );
 
 // NAVIGATORS
 const StackNavigation = StackNavigator({
-  First: { screen: FirstComponent },
-  Second: { screen: SecondComponent },
-  Third: { screen: ThirdComponent }
-});
-
-const DrawerNavigation = DrawerNavigator({
-  First: { screen: FirstComponent },
-  Second: { screen: SecondComponent },
-  Third: { screen: ThirdComponent }
+  FirstStack: { screen: FirstStackComponent },
+  SecondStack: { screen: SecondStackComponent }
 });
 
 const TabNavigation = TabNavigator({
-  First: { screen: FirstComponent },
-  Second: { screen: SecondComponent },
-  Third: { screen: ThirdComponent }
+  FirstTab: { screen: FirstTabComponent },
+  SecondTab: { screen: StackNavigation }
 });
 
 const Root = () => (
   <View style={ styles.container }>
-    {/*<StackNavigation />*/}
-    {/*<DrawerNavigation />*/}
-    {/*<TabNavigation />*/}
+    <TabNavigation />
   </View>
 );
 
