4075c98aadf8af44cc4e6f3e029e8daefa221d76
Vova Porton <vova@500tech.com>
2017-11-29 11:40:35 +0200

    Added basic styling for the rn-playground

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const App = () => (
-  <View>
+  <View style={ styles.container }>
     <Text>
       Hello, course!
     </Text>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default App;
