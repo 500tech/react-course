eb824969db30cf645f64ec6175f0800acad6519f
Boris Dinkevich <your_email@example.com>
2017-11-29 14:53:02 +0200

    Temp

--- src/store.js
+++ src/store.js
@@ -3,7 +3,28 @@ import { createStore } from 'redux';
 const reducer = (state, action) => {
   console.log('ACTION: ' + action.type, action);
 
-  return state;
+  switch (action.type) {
+    case 'ADD':
+      return Object.assign({}, state, {
+        todos: state.todos.concat({
+          title: action.title,
+          completed: false
+        })
+      });
+
+    case 'TOGGLE':
+      const updatedTodos = state.todos.map(todo => todo.title !== action.title
+        ? todo
+        : Object.assign({}, todo, { completed: !todo.completed })
+      );
+
+      return Object.assign({}, state, {
+        todos: updatedTodos
+      });
+
+    default:
+      return state;
+  }
 };
 
 const initialState = {
