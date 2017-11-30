379745ba11e6603747f01b77a46fd64abeb8b39a
Vova Porton <vova@500tech.com>
2017-11-30 14:01:38 +0200

    Added styling to redux

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const App = () => (
-  <View>
+  <View style={ styles.container }>
     <Text>
       Hello, course!
     </Text>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default App;
