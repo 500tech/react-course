ecddf57777e679445e7000d8a069ef94443cd584
Vova Porton <vova@500tech.com>
2018-01-17 17:47:37 +0200

    Dumbed Home down, removed navigation for exercise

--- App.js
+++ App.js
@@ -1,21 +1,5 @@
-import { DrawerNavigator, StackNavigator } from 'react-navigation';
-
 import Home from './Home';
-import Book from './Book';
-import SpecialOffers from './SpecialOffers';
-import PassengersList from './PassengersList';
 
-const HomeStack = StackNavigator({
-  Main: { screen: Home },
-  Book: { screen: Book },
-  SpecialOffers: { screen: SpecialOffers }
-}, {
-  headerMode: 'screen'
-});
-
-const App = DrawerNavigator({
-  Home: { screen: HomeStack },
-  PassengersList: { screen: PassengersList }
-});
+const App = Home;
 
 export default App;
--- Home.js
+++ Home.js
@@ -10,42 +10,18 @@ class Home extends React.Component {
     return (
       <View style={styles.container}>
         <Image style={styles.background} source={require('./assets/background.jpg')}/>
-        <View style={styles.header}>
-          <Image source={require('./assets/el-al.png')} style={styles.logo}/>
-        </View>
+        <View style={styles.header}></View>
 
         <View style={styles.titleContainer}>
           <Text style={styles.title}>
-            What would you like to do?
+            This is a title
           </Text>
         </View>
 
         <View style={styles.buttonsContainer}>
-          <TouchableOpacity style={styles.button}
-                            onPress={() => this.props.navigation.navigate('Book')}>
-            <Image source={require('./assets/plane.png')} style={{ height: 40, width: 40, marginBottom: 20 }}/>
-
-            <Text>Book a flight</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text>Put text here</Text>
           </TouchableOpacity>
-
-          <TouchableOpacity style={styles.button}
-                            onPress={() => this.props.navigation.navigate('SpecialOffers')}>
-            <Image source={require('./assets/tag.png')} style={{ height: 40, width: 40, marginBottom: 20 }}/>
-
-            <Text>Special Offers</Text>
-          </TouchableOpacity >
-
-          <TouchableOpacity style={styles.button}>
-            <Image source={require('./assets/check-in.png')} style={{ height: 40, width: 40, marginBottom: 20 }}/>
-
-            <Text>Express Check-in</Text>
-          </TouchableOpacity >
-
-          <TouchableOpacity style={styles.button}>
-            <Image source={require('./assets/information.png')} style={{ height: 40, width: 40, marginBottom: 20 }}/>
-
-            <Text>Passenger Information</Text>
-          </TouchableOpacity >
         </View>
       </View>
     )
