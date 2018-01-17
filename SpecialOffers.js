d30c3f0608c0d43bd662cef41f7a554c6b98971b
Vova Porton <vova@500tech.com>
2018-01-17 17:52:18 +0200

    Fixed typo

--- SpecialOffers.js
+++ SpecialOffers.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 
 const SpecialOffers = () => (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text style={{ fontSize: 30 }}>BOOK A FLIGHT</Text>
+    <Text style={{ fontSize: 30 }}>SPECIAL OFFERS</Text>
   </View>
 );
 
