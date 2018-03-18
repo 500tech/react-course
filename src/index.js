60702a8c38b46bad99194063f09c43ded5263bc2
Boris Dinkevich <your_email@example.com>
2018-03-18 15:41:58 +0200

    Added basic input

--- src/index.js
+++ src/index.js
@@ -2,7 +2,71 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 
+const Header = ({ title }) => (
+  <h1 onClick={ () => console.log('Yey') }>{ title }</h1>
+);
+
+const Recipe = ({ title }) => (
+  <li>{ title }</li>
+);
+
+class Recipes extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recipes: ['Waffle', 'Omelette', 'Hraime']
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      recipes: this.state.recipes.concat(this.recipeElem.value)
+    });
+
+    this.recipeElem.value = '';
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+            this.state.recipes.map(i => <Recipe key={ i } title={ i }/>)
+          }
+        </ul>
+
+        <form onSubmit={ this.handleSubmit }>
+          <input ref={ (elem) => this.recipeElem = elem } />
+          <button>Add</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const App = () => (
+  <div>
+    <div>
+      <Header title='Recipes:'/>
+      <Recipes/>
+    </div>
+  </div>
+);
+
 render(
-  React.createElement('div', null, 'Hello world!'),
+  <App />,
   document.getElementById('root')
 );
+
+
+
+
+
+
+
+
+
+
