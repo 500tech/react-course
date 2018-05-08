a01f4c50817ab7a0fe3c82def9559f675485c14e
Boris Dinkevich <your_email@example.com>
2018-05-08 14:07:25 +0300

    Added input

--- src/index.js
+++ src/index.js
@@ -2,7 +2,65 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 
+const Header = (props) => (
+  <h1>{ props.text }</h1>
+);
+
+const Recipe = ({ name}) => (
+  <li onClick={ () => console.log('Hey') }>{ name }</li>
+);
+
+class Recipes extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recipes: ['Waffles', 'Omelette', 'Pancake'],
+      name: 'Kipi'
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('Yey: ' + this.state.name);
+
+    this.setState({
+      recipes: this.state.recipes.concat(this.state.name),
+      name: ''
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+            this.state.recipes.map(recipe => <Recipe name={ recipe } key={ recipe }/>)
+          }
+        </ul>
+        <form onSubmit={ this.handleSubmit }>
+          <input value={ this.state.name }
+                 onChange={ (e) => this.setState({ name: e.target.value }) }/>
+          <button>add</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const App = () => (
+  <div>
+    <div>
+      <Header text='Recipes:' />
+      <Recipes />
+    </div>
+  </div>
+);
+
+window.App = App;
+
 render(
-  React.createElement('div', null, 'Hello world!'),
+  App(),
   document.getElementById('root')
 );
