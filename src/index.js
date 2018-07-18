832fb7f2acd6a9be1c07486b17e0f427e785bb7f
Boris Dinkevich <your_email@example.com>
2018-07-18 14:16:53 +0300

    Added input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,65 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 
-render(
-  React.createElement('div', null, 'Hello world!'),
+const Recipe = ({ title }) => (
+  <li>{ title }</li>
+);
+
+const Header = () => (
+  <h1>
+    Recipes:
+  </h1>
+);
+
+
+
+class Recipes extends React.Component {
+  state = {
+    recipes: ['Waffles', 'Omelette', 'Pancake'],
+    title: ''
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({
+      recipes: this.state.recipes.concat(this.state.title),
+      title: ''
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+            this.state.recipes.map(name => <Recipe key={ name } title={ name }/>)
+          }
+        </ul>
+
+        <form onSubmit={ this.handleSubmit.bind(this) }>
+          <input value={ this.state.title }
+                 onInput={ e => this.setState({ title: e.target.value }) }/>
+          <button>add</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const App = () => (
+  <div>
+    <div>
+      <Header />
+      <Recipes a={ 3 }/>
+    </div>
+  </div>
+)
+
+window.App = App;
+
+ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
