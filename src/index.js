4857c92dfb5db9f12e8046d0e63cb057c513ae3b
Shay Keinan <>
2018-07-22 23:43:22 +0300

    init with react 16

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-render(
-  React.createElement('div', null, 'Hello world!'),
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
