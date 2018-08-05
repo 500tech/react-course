a0bf4ecb255c6d72b3ff0f8340dfc72c418984e6
Shay Keinan <>
2018-08-05 08:29:42 +0300

    update files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  React.createElement('h3', null, 'hello React & Redux course'),
+  document.getElementById('root')
+);
 registerServiceWorker();
