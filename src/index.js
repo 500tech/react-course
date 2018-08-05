80ede9c2fe27b875bd02df0e82d7fe19c779e070
Shay Keinan <>
2018-08-05 12:58:09 +0300

    app base

--- src/index.js
+++ src/index.js
@@ -3,8 +3,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => (
+  <div className="app">
+
+    <div className="navigation">
+      <div className="logo">timerApp</div>
+      <div className="avatar" />
+    </div>
+
+    <div className="header">
+      <input className="input" placeholder="timer description" />
+      <div className="button">add new</div>
+    </div>
+
+    <div className="list">
+      <div className="item">
+        <div className="left-area">
+          <div className="date">October 12, 2018</div>
+          <div className="label">Learn the basics of React</div>
+
+          <div className="actions">
+            <div className="delete">delete</div>
+            <div className="favorite">favorite</div>
+          </div>
+        </div>
+        <div className="right-area">
+          <div className="time">12:40</div>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="left-area">
+          <div className="date">October 12, 2018</div>
+          <div className="label">Learn Redux</div>
+
+          <div className="actions">
+            <div className="delete">delete</div>
+            <div className="favorite">favorite</div>
+          </div>
+        </div>
+        <div className="right-area">
+          <div className="time">12:40</div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 ReactDOM.render(
-  React.createElement('h3', null, 'hello React & Redux course'),
+  <App />,
   document.getElementById('root')
 );
 registerServiceWorker();
