aa77d36f5f996556f038b67e4c8e223e7b7e6fb2
Shay Keinan <>
2018-08-05 14:20:45 +0300

    split into comps

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Navigation from './Navigation';
+import Header from './Header';
+import List from './List';
+
 const App = () => (
-  <div></div>
+  <div className="app">
+    <Navigation />
+    <Header />
+    <List />
+  </div>
 );
 
 export default App;
