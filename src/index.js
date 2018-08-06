506251b839f68732450b631d9b6be451adf4c2f9
Shay Keinan <>
2018-08-06 11:16:39 +0300

    changes - complete todos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import './index.css';
-
-const data = [
-  { id: 0, label: 'learn React', isChecked: true },
-  { id: 1, label: 'learn Redux', isChecked: false },
-  { id: 2, label: 'server communication', isChecked: false }
-];
 
 const Item = (props) => (
   <li className="completed">
@@ -21,45 +15,83 @@ const Item = (props) => (
   </li>
 );
 
-const App = () => (
-  <div>
-    <section className="todoapp">
+class App extends React.Component {
+  state = {
+    data: [
+      { id: 0, label: 'learn React', isChecked: true },
+      { id: 1, label: 'learn Redux', isChecked: false },
+      { id: 2, label: 'server communication', isChecked: false }
+    ]
+  };
 
-      <header className="header">
-        <h1>todos</h1>
-        <input className="new-todo" placeholder="What needs to be done?" autoFocus />
-      </header>
+  uuid() {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 
-      <section className="main">
-        <input className="toggle-all" type="checkbox" />
-        <ul className="todo-list">
-          {data.map(item => <Item key={item.id} {...item} />)}
-        </ul>
-      </section>
+    return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+  }
 
-      <footer className="footer">
-        <span className="todo-count"><strong>0</strong> item left</span>
-        <ul className="filters">
-          <li>
-            <a className="selected" href="#/">All</a>
-          </li>
-          <li>
-            <a href="#/active">Active</a>
-          </li>
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
-        </ul>
-        <button className="clear-completed">Clear completed</button>
-      </footer>
-    </section>
+  handleKeyUp = (e) => {
+    if (e.which === 13) {
+      this.setState({
+        data: this.state.data.concat({
+          id: this.uuid(),
+          label: e.target.value,
+          isChecked: false
+        })
+      }, () => {
+        this.input.value = '';
+      });
+    }
+  };
 
-    <footer className="info">
-      <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
-      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-    </footer>
-  </div>
-);
+  render() {
+    return (
+      <Fragment>
+        <section className="todoapp">
+
+          <header className="header">
+            <h1>todos</h1>
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              autoFocus
+              onKeyUp={this.handleKeyUp}
+              ref={el => this.input = el}
+            />
+          </header>
+
+          <section className="main">
+            <input className="toggle-all" type="checkbox" />
+            <ul className="todo-list">
+              {this.state.data.map(item => <Item key={item.id} {...item} />)}
+            </ul>
+          </section>
+
+          <footer className="footer">
+            <span className="todo-count"><strong>0</strong> item left</span>
+            <ul className="filters">
+              <li>
+                <a className="selected" href="#/">All</a>
+              </li>
+              <li>
+                <a href="#/active">Active</a>
+              </li>
+              <li>
+                <a href="#/completed">Completed</a>
+              </li>
+            </ul>
+            <button className="clear-completed">Clear completed</button>
+          </footer>
+        </section>
+
+        <footer className="info">
+          <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+          <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+        </footer>
+      </Fragment>
+    );
+  }
+}
 
 render(
   <App />,
