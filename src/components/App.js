4d019ba6f8f5af3fdb5bd87988ea91633d2f6202
Shay Keinan <>
2018-10-03 15:11:43 +0300

    class comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+// comps
 import Button from './common/Button';
 import Input from './common/Input';
 import List from './List';
