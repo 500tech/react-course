3c50f981928b62cd8fd379dac3ba60749b63434d
Shay Keinan <>
2018-10-04 12:02:14 +0300

    update styles

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import moment from 'moment';
+import Fade from 'react-reveal/Fade';
 
 export default class Timer extends React.Component {
   state = {
@@ -24,9 +25,14 @@ export default class Timer extends React.Component {
     const { time } = this.state;
 
     return (
-      <Container key={id}>
-        {label} | delete | show | {time.format('mm:ss')}
-      </Container>
+      <Fade top>
+        <Container key={id}>
+          <Label>{label}</Label>
+          <Label color="red" clickable>delete</Label>
+          <Label color="blue" clickable>show</Label>
+          <Label>{time.format('mm:ss')}</Label>
+        </Container>
+      </Fade>
     );
   }
 }
@@ -34,11 +40,25 @@ export default class Timer extends React.Component {
 const Container = styled.div`
   width: 100%;
   height: 30px;
-  background: #dadada;
+  background: #fafafa;
+  border-radius: 4px;
   margin-bottom: 10px;
   box-sizing: border-box;
   display: flex;
   align-items: center;
   text-transform: capitalize;
   padding: 0 20px;
+  justify-content: space-between;
+`;
+
+const Label = styled.div`
+  color: ${({ color }) => color || '#444'};
+  
+  ${({ clickable }) => clickable && css`
+    cursor: pointer;
+    
+    &:hover {
+      opacity: 0.6;
+    }
+  `}
 `;
