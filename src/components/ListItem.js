de80be00b780bd3a43d499d223ec52d5b10e3498
Shay Keinan <>
2018-10-24 15:29:39 +0300

    change detection

--- src/components/ListItem.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const ListItem = props =>
-  (<li>{props.label}</li>);
-
-export default ListItem;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+
+class App extends React.Component {
+  state = {
+    label: 'foo'
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ label: 'hello oracle '});
+    }, 3000);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header label={this.state.label} />
+      </div>
+    );
+  }
+}
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h3>{this.props.label}</h3>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
