caa0040d26362a7ca168fd1eb7b9cf34fefeba78
Shay Keinan <>
2018-10-25 11:42:23 +0300

    build redux from scratch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
+
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+class createStore {
+  constructor(rootReducer) {
+    this.rootReducer = rootReducer;
+
+    this.listeners = [];
+
+    this.dispatch({ type: '@@INIT_REDUX' });
+  }
+
+  subscribe(callback) {
+    this.listeners.push(callback);
+  }
+
+  dispatch(action) {
+    this.state = this.rootReducer(this.state, action);
+
+    this.listeners.forEach(listener => listener());
+  }
+
+  getState() {
+   return this.state;
+  }
+}
+
+const store = new createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
 
 ReactDOM.render(
   <App />,
