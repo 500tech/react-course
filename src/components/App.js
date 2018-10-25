1d9eda7f2cc6140d39ba4073b6265d15ee47b317
Shay Keinan <>
2018-10-25 16:16:10 +0300

    content component

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,18 @@ import { Provider } from 'react-redux';
 import store from '../redux/store';
 import Counter from './Counter';
 import Demo from './Demo';
+import Content from './Content';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <div>
-          <Demo />
-          <Counter />
-        </div>
+        <Content>
+          <div>
+            <Demo />
+            <Counter />
+          </div>
+        </Content>
       </Provider>
     );
   }
---
+++ src/components/Content.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+
+class Content extends React.Component {
+  componentDidUpdate(prevProps) {
+
+    if (!this.props.token && prevProps.token) {
+      this.props.history.push('/login');
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div>{children}</div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  token: state.currentUser.token
+});
+
+export default withRouter(connect(
+  mapStateToProps
+)(Content));
