e35a5127fde06c04edca70b8e5f11b1bd27bbdfc
Shay Keinan <>
2018-11-15 14:04:24 +0200

    change detection

--- src/index.js
+++ src/index.js
@@ -2,29 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 
-const items = [
-  { id: 0, label: 'eggs' },
-  { id: 1, label: 'bread' },
-  { id: 2, label: 'milk' }
-];
+class App extends React.Component {
+  state = {
+    data: [
+      { id: 0, label: 'item 0' },
+      { id: 1, label: 'item 1' },
+    ]
+  };
 
-const App = () => (
-  <div className="list">
-    {
-      items.map(item => (
-        <div
-          className="item"
-          key={item.id}>
-          {item.label}
-        </div>
-      ))
-    }
-  </div>
-);
+  addItem = () => {
+    this.setState({
+      data: [
+        ...this.state.data,
+        { id: 6, label: 'untitled' }
+      ]
+    })
+  };
 
+  removeItem(id) {
+    this.setState({
+      data: this.state.data.filter(item => item.id !== id)
+    })
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className="list">
+        <div className="button" onClick={this.addItem}>add item</div>
+
+        {
+          data.length ? (
+            data.map(item => (
+              <div
+                className="item"
+                key={item.id}
+                onClick={() => this.removeItem(item.id)}
+              >
+                {item.label}
+              </div>
+            ))
+          ) : 'empty!'
+
+        }
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
 
