0708606e8a36772e39faadccbafd5ac6c1803246
Shay Keinan <>
2018-11-18 10:19:01 +0200

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,10 @@ import Timers from './Timers';
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/timers/:id?" component={Timers} />
+      <Route
+        exact
+        path="/timers/:id?"
+        component={Timers} />
     </Switch>
   </Router>
 );
