8f4214a4df5fb4ac876dfdb326ad1f86e8513167
Shay Keinan <>
2018-12-09 15:20:36 +0200

    css changes

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -14,9 +14,12 @@ const Container = styled.input`
   width: 300px;
   height: 34px;
   flex: 1;
-  border: 1px solid #444;
+  border: 1px solid #dadada;
   box-sizing: border-box;
   padding: 0 20px;
   font-size: 12px;
   margin-right: 10px;
+  transition: all 300ms;
+  
+  &:focus { border-color: #444; }
 `;
